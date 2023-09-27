f03f65e289e0a325f56cd782e826f1960fa17167
Adam Welin <welinadam@users.noreply.github.com>
2023-09-27 17:24:54 +0200

    Data table required

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-
 require_once __DIR__."/header.php"; ?>
 
 <h1>Some Teams</h1>
 
-
-<?php require_once __DIR__."/footer.php"; ?>
+<?php 
+require_once __DIR__."/table.php";
+require_once __DIR__."/footer.php";?>
